98a3263114a606b2a56c3bb1c82653c39b76c8a9
ndele2024 <romualdasonmene@gmail.com>
2025-01-18 20:53:14 -0500

    deploy commit 2025-01-18

--- src/components/livraison/livrason.js
+++ src/components/livraison/livrason.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const livrason = () => {
     return (
-        <div>
-            
+        <div class='liv'>
+            COMMANDE EFFECTUER
         </div>
     );
 };
